e9f037d3ed24507d281f753c310b437e73538ad5
AliTaha27170 <ali.taha27191@gmail.com>
2022-04-18 03:09:40 +0300

    Auth and Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,32 +17,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    /**
+     * Home Routes
+     */
+    Route::get('/', [HomeController::class,'index'])->name('home.index');
 
 Route::get('/index', function () {
     return view('index');
 });
 
+Route::group(['namespace' => 'App\Http\Controllers\Auth'], function()
+{
 
-Route::get('/food-products', function () {
-    return view('foods');
+
+    Route::group(['middleware' => ['guest']], function() {
+        /**
+         * Register Routes
+         */
+        Route::get('/register', [RegisterController::class , 'show'])->name('register.show');
+        Route::post('/register', [RegisterController::class , 'register'])->name('register.perform');
+
+        /**
+         * Login Routes
+         */
+        Route::get('/login', [LoginController::class,'show'])->name('login');
+        Route::post('/login', [LoginController::class,'login'])->name('login.perform');
+
+    });
+
+    Route::group(['middleware' => ['auth']], function() {
+        /**
+         * Logout Routes
+         */
+        Route::get('/logout', [LogoutController::class ,'perform'])->name('logout.perform');
+    });
 });
-
-Route::get('/cookbooks', function () {
-    return view('cookbooks');
-});
-
-Route::get('/products-listing', function () {
-    return view('products-listing');
-});
-
-Route::get('/about-us', function () {
-    return view('about');
-});
-
-Route::get('/contact-us', function () {
-    return view('contact');
-});
-
